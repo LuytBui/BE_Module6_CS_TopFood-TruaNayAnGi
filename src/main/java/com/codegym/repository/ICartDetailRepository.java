59063b12ecffd6ace6177776f025e8f71f9f4066
LuytBui <luytbq43@gmail.com>
2022-05-13 15:57:54 +0700

    lam do tinh nang mua hang

--- src/main/java/com/codegym/repository/ICartDetailRepository.java
+++ src/main/java/com/codegym/repository/ICartDetailRepository.java
@@ -6,4 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ICartDetailRepository extends JpaRepository<CartDetail, Long> {
     Iterable<CartDetail> findAllByUser(User user);
+
+    void deleteAllByUser(User user);
+
+    CartDetail findFirstByUser(User user);
 }
--- src/main/java/com/codegym/service/cart/CartService.java
+++ src/main/java/com/codegym/service/cart/CartService.java
@@ -3,6 +3,8 @@ package com.codegym.service.cart;
 import com.codegym.model.dto.cart.CartDetailDto;
 import com.codegym.model.dto.cart.CartDto;
 import com.codegym.model.entity.CartDetail;
+import com.codegym.model.entity.Merchant;
+import com.codegym.model.entity.dish.Dish;
 import com.codegym.model.entity.user.User;
 import com.codegym.repository.ICartDetailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,10 @@ public class CartService{
         return this.cartDetailRepository.findAllByUser(user);
     }
 
+    /*
+        Lấy thông tin về giỏ hàng của user
+        Thông tin gửi về dưới dạng đối tượng CartDto
+     */
     public CartDto getUserCartDto(User user){
         CartDto cartDto = new CartDto();
         cartDto.setUser(user);
@@ -30,5 +36,49 @@ public class CartService{
         );
 
         return cartDto;
+    }
+
+    /*        Xóa toàn bộ các món ăn trong cart của user     */
+    public void emptyCart(User user){
+        this.cartDetailRepository.deleteAllByUser(user);
+    }
+
+    /*
+        Thêm một món ăn vào giỏ hàng
+        Nếu món ăn khác cửa hàng với những món trong giỏ thì xóa hết món trong giỏ
+     */
+    public CartDto addDishToCart(User user, Dish dish, int quantity){
+        // Nếu khác CH thì xóa toàn bộ giỏ
+        CartDetail firstInCart = cartDetailRepository.findFirstByUser(user);
+        if (firstInCart != null){
+            Merchant oldMerchant = firstInCart.getDish().getMerchant();
+            Merchant newMerchant = dish.getMerchant();
+            boolean sameMerchant = oldMerchant.getId().equals(newMerchant.getId());
+            if (!sameMerchant){
+                emptyCart(user);
+            }
+        }
+
+        // Nếu trong giỏ đã có dish này thì tăng số lượng
+        Iterable<CartDetail> cartDetails = cartDetailRepository.findAllByUser(user);
+        cartDetails.forEach(
+                cartDetail -> {
+                    boolean sameDish = cartDetail.getDish().getId().equals(dish.getId());
+                    if (sameDish) {
+                        int oldQuantity = cartDetail.getQuantity();
+                        cartDetail.setQuantity();
+                    }
+                }
+        );
+
+        // Tạo đối tượng CartDetail mới và lưu vào DB
+        CartDetail cartDetail = new CartDetail();
+        cartDetail.setUser(user);
+        cartDetail.setDish(dish);
+        cartDetail.setQuantity(quantity);
+        cartDetailRepository.save(cartDetail);
+
+        // Lấy thông tin cart từ DB để trả về
+        return getUserCartDto(user);
     }
 }
